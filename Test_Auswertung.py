44531d4c603cd712eab281ae73f7d919e6863236
Alexander Hoppe <hoppe101005@gmail.com>
2026-03-12 13:20:02 +0100

    Maske V1.0 ohne Syntaxfehler

---
+++ Test_Auswertung.py
@@ -0,0 +1,7 @@
+############
+### Test ###
+############
+
+import Auswertung_Zug
+arrey = [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1] #Initialisierung des Spielerarreys, damit die Punkte in der Hauptschleife weiterverwendet werden können
+print(Auswertung_Zug(Zug(1,2,3,4,5),arrey)) #Test der Funktion Auswertung_Zug mit einem Beispielzug
