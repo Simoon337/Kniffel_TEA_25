19cdce3cc2188ab698aec10f18c3aeacd50f9e6c
ToBu-05 <tobu.info@gmail.com>
2026-03-15 18:09:58 +0100

    Ausgabe für Endpunktestandstabelle und Gewinner hinzugefügt

--- player.py
+++ player.py
@@ -57,4 +57,106 @@ def print_score_list(player_name, score_list):
     """)
 
 #Testaufruf der print_score_list Funktion
-print_score_list("Max", [0, 6, None, 12, 5, 18, 0, 20, 30, None, 30, 40, None, 22, 315])
+#print_score_list("Max", [0, 6, None, 12, 5, 18, 0, 20, 30, None, 30, 40, None, 22, 315])
+
+
+#Gibt die Endtabelle mit den Spielernamen und ihren Punkteständen aus (score_lists ist eine Liste von Listen, die die Punktestände aller Spieler enthält)
+def print_final_score(player_names, score_lists):
+    number_of_players = len(player_names)           #Anzahl der Spieler
+    space_for_number = [0] * number_of_players      #Liste, die die Anzahl der Leerzeichen für jeden Spielernamen enthält, damit die Punkte in der Endtabelle ordentlich ausgerichtet sind
+    space_for_name = [0] * number_of_players        #Liste, die die Anzahl der Leerzeichen für jeden Spielernamen enthält, damit die Namen in der Endtabelle ordentlich ausgerichtet sind
+
+    for i in range(number_of_players):              #Wertet aus, wie viele Leerzeichen für jeden Spielernamen vor diesem und den Punkten in der Endtabelle stehen
+        length_name = len(player_names[i])
+        if length_name < 10:
+            space_for_number[i] = 7                 #Die Anzahl derLeerzeichen inklusive bis zu drei Ziffern soll mindestens 10 betragen
+            space_for_name[i] = 10 - length_name    #Leerzeichen vor dem Namen, damit die Namen in der Endtabelle ordentlich ausgerichtet sind
+        else:
+            space_for_number[i] = length_name - 2   #Wenn der Name 10 Zeichen lang oder länger ist, soll die Anzahl der Leerzeichen immer 3 weniger als die Anzahl der Zeichen im (Namen + ein Leerzeichen) sein
+            space_for_name[i] = 1
+
+    print("Endpunktestand:")                        #Gibt die Endtabelle mit den Spielernamen und ihren Punkteständen aus
+    print("Spieler:     ", end="")
+    for i in range(number_of_players):
+        print(" " * space_for_name[i] + player_names[i] + "|", end="")
+    print("\nEinser:      ", end="")
+    for i in range(number_of_players):
+        print(" " * space_for_number[i] + convert_to_print(score_lists[15 * i]) + "|", end="")
+    print("\nZweier:      ", end="")
+    for i in range(number_of_players):
+        print(" " * space_for_number[i] + convert_to_print(score_lists[15 * i + 1]) + "|", end="")
+    print("\nDreier:      ", end="")
+    for i in range(number_of_players):
+        print(" " * space_for_number[i] + convert_to_print(score_lists[15 * i + 2]) + "|", end="")
+    print("\nVierer:      ", end="")
+    for i in range(number_of_players):
+        print(" " * space_for_number[i] + convert_to_print(score_lists[15 * i + 3]) + "|", end="")
+    print("\nFünfer:      ", end="")
+    for i in range(number_of_players):
+        print(" " * space_for_number[i] + convert_to_print(score_lists[15 * i + 4]) + "|", end="")
+    print("\nSechser:     ", end="")
+    for i in range(number_of_players):
+        print(" " * space_for_number[i] + convert_to_print(score_lists[15 * i + 5]) + "|", end="")
+    print("\nBonus:       ", end="")
+    for i in range(number_of_players):
+        print(" " * space_for_number[i] + convert_to_print(score_lists[15 * i + 6]) + "|", end="")
+    print("\nDreierpasch: ", end="")
+    for i in range(number_of_players):
+        print(" " * space_for_number[i] + convert_to_print(score_lists[15 * i + 7]) + "|", end="")
+    print("\nViererpasch: ", end="")
+    for i in range(number_of_players):
+        print(" " * space_for_number[i] + convert_to_print(score_lists[15 * i + 8]) + "|", end="")
+    print("\nFull House:  ", end="")
+    for i in range(number_of_players):
+        print(" " * space_for_number[i] + convert_to_print(score_lists[15 * i + 9]) + "|", end="")
+    print("\nkl. Straße:  ", end="")
+    for i in range(number_of_players):
+        print(" " * space_for_number[i] + convert_to_print(score_lists[15 * i + 10]) + "|", end="")
+    print("\ngr. Straße:  ", end="")
+    for i in range(number_of_players):
+        print(" " * space_for_number[i] + convert_to_print(score_lists[15 * i + 11]) + "|", end="")
+    print("\nKniffel:     ", end="")
+    for i in range(number_of_players):
+        print(" " * space_for_number[i] + convert_to_print(score_lists[15 * i + 12]) + "|", end="")
+    print("\nChance:      ", end="")
+    for i in range(number_of_players):
+        print(" " * space_for_number[i] + convert_to_print(score_lists[15 * i + 13]) + "|", end="")
+    print("\nSumme:       ", end="")
+    for i in range(number_of_players):
+        print(" " * space_for_number[i] + convert_to_print(score_lists[15 * i + 14]) + "|", end="")
+    print()
+
+#Testaufruf der print_final_score Funktion
+#print_final_score(["Jan", "Anna", "Maximilian"], [0, 6, 5, 12, 5, 18, 0, 20, 30, 0, 30, 40, 0, 22, 315, 4, 2, 9, 8, 10, 30, 35, 18, 24, 0, 25, 35, 0, 20, 300, 3, 4, 0, 16, 25, 30, 35, 0, 0, 25, 30, 40, 50, 26, 281])
+
+#Wertet den Gewinner oder die Gewinner aus und gibt die Namen der Gewinner und ihre Punktzahl aus
+def winner(player_names, score_lists):
+    max_score = 0
+    winner_index = 0
+    number_of_winners = 1
+
+    for i in range(len(player_names)):                      #Findet die höchste Gesamtpunktzahl und den Index des Gewinners
+        if score_lists[15 * i + 14] > max_score:
+            max_score = score_lists[15 * i + 14]
+            winner_index = i
+            number_of_winners = 1
+        elif score_lists[15 * i + 14] == max_score:         #Prüfung, ob es mehrere Gewinner gibt
+            number_of_winners += 1
+
+    if number_of_winners > 1:                               #Wenn es mehr als einen Gewinner gibt, werden Alle Namen und ihre Punktzahl ausgegeben
+        print("Es haben ", end="")
+        printed_winners_counter = 0                         #Variable zum zählen, wie viele Gewinner bereits ausgegeben wurden, damit die Namen der Gewinner mit ", " oder " und " getrennt werden können
+        for i in range(len(player_names)):
+            if score_lists[15 * i + 14] == max_score:
+                printed_winners_counter += 1
+                print(player_names[i], end="")
+                if printed_winners_counter < number_of_winners - 1:
+                    print(", ", end="")
+                elif printed_winners_counter < number_of_winners:
+                    print(" und ", end="")
+        print(f" mit {max_score} Punkten gewonnen!")
+    else:                                                   #Wenn es einen Gewinner gibt, wird nur der Name dieses Gewinners und seine Punktzahl ausgegeben
+        print(f"Der Gewinner ist {player_names[winner_index]} mit {max_score} Punkten!")
+
+#Testaufruf der winner Funktion
+#winner(["Jan", "Anna", "Maximilian"], [0, 6, 5, 12, 5, 18, 0, 20, 30, 0, 30, 40, 0, 22, 314, 4, 2, 9, 8, 10, 30, 35, 18, 24, 0, 25, 35, 0, 20, 314, 3, 4, 0, 16, 25, 30, 35, 0, 0, 25, 30, 40, 50, 26, 315])
