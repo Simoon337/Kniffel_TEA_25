80e4e4a7446894a19d548a7945b8b569c5793a01
Alexander Hoppe <hoppe101005@gmail.com>
2026-03-19 13:49:06 +0100

    gelöschte Dateien

--- Test_Auswertung.py
+++
@@ -1,7 +0,0 @@
-############
-### Test ###
-############
-
-from Auswertung_Zug import Auswertung_Zug 
-arrey = [None, None, None, None, None, None, None, None, None, None, None, None, None]  # Initialisierung des Spielerarreys (None = noch nicht belegt)
-print(Auswertung_Zug(1,2,3,4,5,arrey)) #Test der Funktion Auswertung_Zug mit einem Beispielzug
